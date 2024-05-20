84d692e3284a8de66efa9a7f96b683041acebee3
sor999 <sorsor999@naver.com>
2024-05-20 09:35:30 +0900

    feat: Todo 리스트 CRUD

--- src/App.js
+++ src/App.js
@@ -1,21 +1,57 @@
 import React from 'react';
-import Hello from './Hello';
 import Todo from './Todo';
+import AddTodo from './AddTodo';
+import { Paper, List, Container } from '@material-ui/core';
 import './App.css';
+
+
 
 class App extends React.Component { // props와 state를 이용해 매개변수 처리하기
   constructor(props){ // 매개변수 props 생성자. constructor키워드로 생성
     super(props); // pros 초기화
     this.state = { // item 에 item.id, item.title, item.done 이름과 값 할당
-      item :{id:0, title:"Hello World 1 ", done:true},
+      items :[ 
+        {id:"todo0", title:"Todo 1 ", done:true},
+        {id:"todo1", title:"Todo 2 ", done:false}, 
+      ],
     };
   }
+  //add 추가
+  add = (item) => {
+    const thisItems = this.state.items;
+    item.id = "ID-" + thisItems.length; 
+    item.done = false;
+    thisItems.push(item);
+    this.setState({ items: thisItems });
+    console.log("items:", this.state.items);
+  }
+  // delete
+  delete = (item) => {
+    const thisItems = this.state.items;
+    const newItems = thisItems.filter(e => e.id !== item.id);
+    this.setState({ items: newItems }, () => {
+      console.log("Updated Items: ", this.state.items);
+    });
+  }
 
-  render(){
-    return ( // 매개변수 item에 변수명/ 값 전달 
+  render() {
+    var todoItems = this.state.items.length > 0 && (
+      <Paper style={{ margin: 16 }}>
+        <List>
+          {this.state.items.map((item, idx) => (
+            <Todo item={item} key={item.id} delete={this.delete} />
+          ))}
+        </List>
+      </Paper>
+    );
+
+    return (
       <div className="App">
-        <Todo item={this.state.item}/> 
-      </div> // 컴포넌트 불러올 때 파일 이름 써주면 됨(Todo)
+        <Container maxWidth="md">
+          <AddTodo add={this.add} />
+          <div className="TodoList">{todoItems}</div>
+        </Container>
+      </div>
     );
   }
 }
--- src/Hello.js
+++ src/Hello.js
@@ -1,12 +1,12 @@
-import React from 'react';
-
-class Hello extends React.Component {
-    render(){
-    return (
-        <div className="Hello">
-            <label for="hello"> 안녕하세요!</label>
-        </div>
-    );
-    }
-}
-export default Hello;
+// import React from 'react';
+// //1. 안녕하세요를 출력하는 예제 
+// class Hello extends React.Component {
+//     render(){
+//     return (
+//         <div className="Hello">
+//             <label for="hello"> 안녕하세요!</label>
+//         </div>
+//     );
+//     }
+// }
+// export default Hello;
